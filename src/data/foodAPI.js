39704def845531440e40a4e3b8c2b5a2f7b1f847
malarcon <malarcon@SOPORTE_TI-PC>
2023-12-07 14:34:20 -0500

    Link a la publicacion

--- src/data/foodAPI.js
+++ src/data/foodAPI.js
@@ -11,7 +11,7 @@ export const getNewPublications = async (page) => {
 }
 
 export const getPublication = async (id) => {
-    const response = await fetch(`https://apitestprueba-4fg7.onrender.com/publicaciones??id=${id}`);
+    const response = await fetch(`https://apitestprueba-4fg7.onrender.com/publicaciones?id=${id}`);
     const result = await response.json();
     return result;
 }
--- src/pages/PublicationPage.jsx
+++ src/pages/PublicationPage.jsx
@@ -1,12 +1,31 @@
+import { useLoaderData } from 'react-router-dom';
+import { getPublication } from '../data/foodAPI';
+import '../styles/pages.css'
 
 export const loaderPublication = async ({ params }) => {
-    console.log(params)
-    return {}
+  const { id } = params;
+  const publication = await getPublication(id);
+  return {
+    data: publication[0]
+  }
 }
 
 const PublicationPage = () => {
+  const { data } = useLoaderData();
+  const { id, nombre, fechapub, creadopor, descripcion, imagen } = data;
+  
   return (
-    <div>PublicationPage</div>
+    <section className="seccion__publicacion contenedor">
+      <div className="publicacion__contenido">
+        <h2 className="publicacion__titulo">{nombre}</h2>
+        <div className="publicacion__informacion">
+          <div className="publicacion__imagen__contenedor">
+            <img src={imagen} alt="imagen puvlicacion" />
+          </div>
+          <p className="publicacion__descripcion">{descripcion}</p>
+        </div>
+      </div>
+    </section>
   )
 }
 
